faffbbfab842d7fb1096386a154e88d3d7386332
prjct0192 <project.nemesis92@gmail.com>
2020-09-25 11:18:51 +0300

    added todo component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {FormsModule} from '@angular/forms';
 import { ToDoListComponent } from './to-do-list/to-do-list.component';
+import { ToDoContComponent } from './to-do-cont/to-do-cont.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ToDoListComponent,
+    ToDoContComponent,
   ],
   imports: [
     BrowserModule,
